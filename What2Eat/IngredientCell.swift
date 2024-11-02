d8b64f6fb0f90e992d7f58bb27e0d477661fea48
Himavarshith Reddy <himavarshithreddy@gmail.com>
2024-11-02 15:44:57 +0530

    productscreens

---
+++ What2Eat/IngredientCell.swift
@@ -0,0 +1,17 @@
+//
+//  IngredientCell.swift
+//  What2Eat
+//
+//  Created by admin68 on 02/11/24.
+//
+
+import UIKit
+
+class IngredientCell: UITableViewCell {
+
+    @IBOutlet weak var ingredientLabel: UILabel!
+    
+    @IBOutlet weak var riskLevelLabel: UILabel!
+    
+    
+}
